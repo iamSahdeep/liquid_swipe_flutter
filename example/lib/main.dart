643650ffeceaf46782c6cdbea43a820f64fc50c8
Sahdeep Singh <sahdeepsingh98@gmail.com>
2021-05-29 01:24:36 +0530

    example update

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,7 +12,7 @@ void main() {
 }
 
 ///Class to hold data for itembuilder in Withbuilder app.
-class ItemData{
+class ItemData {
   final Color color;
   final String image;
   final String text1;
@@ -35,11 +35,14 @@ class _WithBuilder extends State<WithBuilder> {
 
   List<ItemData> data = [
     ItemData(Colors.blue, "assets/1.png", "Hi", "It's Me", "Sahdeep"),
-    ItemData(Colors.deepPurpleAccent, "assets/1.png", "Take a", "Look At", "Liquid Swipe"),
+    ItemData(Colors.deepPurpleAccent, "assets/1.png", "Take a", "Look At",
+        "Liquid Swipe"),
     ItemData(Colors.green, "assets/1.png", "Liked?", "Fork!", "Give Star!"),
-    ItemData(Colors.yellow, "assets/1.png", "Can be", "Used for", "Onboarding design"),
+    ItemData(Colors.yellow, "assets/1.png", "Can be", "Used for",
+        "Onboarding design"),
     ItemData(Colors.red, "assets/1.png", "Do", "try it", "Thank you"),
   ];
+
   @override
   void initState() {
     liquidController = LiquidController();
@@ -77,8 +80,9 @@ class _WithBuilder extends State<WithBuilder> {
           children: <Widget>[
             LiquidSwipe.builder(
               itemCount: data.length,
-              itemBuilder: (context, index){
+              itemBuilder: (context, index) {
                 return Container(
+                  width: double.infinity,
                   color: data[index].color,
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.center,
@@ -87,7 +91,8 @@ class _WithBuilder extends State<WithBuilder> {
                     children: <Widget>[
                       Image.asset(
                         data[index].image,
-                        fit: BoxFit.cover,
+                        height: 400,
+                        fit: BoxFit.contain,
                       ),
                       Padding(
                         padding: EdgeInsets.all(20.0),
@@ -152,10 +157,9 @@ class _WithBuilder extends State<WithBuilder> {
                 child: FlatButton(
                   onPressed: () {
                     liquidController.jumpToPage(
-                        page:
-                            liquidController.currentPage + 1 > data.length - 1
-                                ? 0
-                                : liquidController.currentPage + 1);
+                        page: liquidController.currentPage + 1 > data.length - 1
+                            ? 0
+                            : liquidController.currentPage + 1);
                   },
                   child: Text("Next"),
                   color: Colors.white.withOpacity(0.01),
@@ -174,7 +178,6 @@ class _WithBuilder extends State<WithBuilder> {
     });
   }
 }
-
 
 ///Example of App with LiquidSwipe by providing list of widgets
 class WithPages extends StatefulWidget {
@@ -398,7 +401,6 @@ class _WithPages extends State<WithPages> {
           children: <Widget>[
             LiquidSwipe(
               pages: pages,
-              positionSlideIcon: 0.8,
               slideIconWidget: Icon(Icons.arrow_back_ios),
               onPageChangeCallback: pageChangeCallback,
               waveType: WaveType.liquidReveal,
